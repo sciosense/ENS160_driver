b548df9ef9f22ed9e04c59319ac8d80b29d123d0
Sciosense <38984838+sciosense@users.noreply.github.com>
2020-04-29 10:21:02 +0200

    Update ens160basic_custom.ino

--- examples/ens160basic_custom/ens160basic_custom.ino
+++ examples/ens160basic_custom/ens160basic_custom.ino
@@ -1,7 +1,7 @@
 /***************************************************************************
   This is a example for ENS160 basic reading 
     
-  Written by Christoph Friese for Sciosense / 8-April-2020
+  Written by Christoph Friese for Sciosense / 29-April-2020
  ***************************************************************************/
 
 #include <Wire.h>
@@ -13,7 +13,7 @@ int ArduinoLED = 13;
 //-------------------------------------------------------------
 #include "ScioSense_ENS160.h"  // ENS160 library
 ScioSense_ENS160      ens160(ENS160_I2CADDR_0);
-//sciosense_ENS160      ens160(ENS160_I2CADDR_1);
+//ScioSense_ENS160      ens160(ENS160_I2CADDR_1);
 
 /*--------------------------------------------------------------------------
   SETUP function
@@ -53,8 +53,10 @@ void setup() {
     }
 
     if (ArduinoIDE) Serial.print("setup: ENS160 custom mode ");
-    ens160.initCustomMode(1);
-    ens160.addCustomStep(1000, 1, 1, 1, 1, 250, 350, 350, 325);
+    ens160.initCustomMode(3);                                     // Create custom sequence with three steps
+    ens160.addCustomStep(50, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 50ms, no measurments, all hotplates at low temperatures 
+    ens160.addCustomStep(350, 1, 1, 1, 1, 160, 215, 215, 200);    // Step 2: 350ms, no measurments, all hotplates at medium temperatures 
+    ens160.addCustomStep(600, 1, 1, 1, 1, 250, 350, 350, 325);    // Step 3: 600ms, measurments done, all hotplates at high temperatures 
     if (!ens160.setMode(ENS160_OPMODE_CUSTOM) ) {
       if (ArduinoIDE) Serial.println("FAILED");
     } else {
