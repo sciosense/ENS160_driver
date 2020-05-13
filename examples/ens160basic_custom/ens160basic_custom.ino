25ec60543bc5b18fc2a65088afc7c36dd222260b
Sciosense <38984838+sciosense@users.noreply.github.com>
2020-05-13 16:37:15 +0200

    Update ens160basic_custom.ino
    Remove "ArduinoIDE" option to prevent serial output

--- examples/ens160basic_custom/ens160basic_custom.ino
+++ examples/ens160basic_custom/ens160basic_custom.ino
@@ -5,7 +5,6 @@
  ***************************************************************************/
 
 #include <Wire.h>
-#define ArduinoIDE true // false for raspi logging; true waits for serial monitor before any action happens
 int ArduinoLED = 13;
 
 //-------------------------------------------------------------
@@ -34,33 +33,30 @@ void setup() {
   pinMode(ArduinoLED, OUTPUT);
   digitalWrite(ArduinoLED, LOW);
 
-  if (ArduinoIDE) {
-    Serial.println("ENS160 example");
-    delay(1000);
-  }
+  Serial.println("ENS160 example");
+  delay(1000);
 
   //-------------------------------------------------------------
   //ENS160 related items
   //-------------------------------------------------------------
-  if (ArduinoIDE) Serial.print("ENS160...");
-  ok = ens160.begin();if (ArduinoIDE) Serial.println(ens160.available() ? "done." : "failed!");
+  Serial.print("ENS160...");
+  ok = ens160.begin();
+  Serial.println(ens160.available() ? "done." : "failed!");
   if (ens160.available()) {
     // Print ENS160 versions
-    if (ArduinoIDE) {
-      Serial.print("Rev: "); Serial.print(ens160.getMajorRev());
-      Serial.print("."); Serial.print(ens160.getMinorRev());
-      Serial.print("."); Serial.println(ens160.getBuild());
-    }
+    Serial.print("Rev: "); Serial.print(ens160.getMajorRev());
+    Serial.print("."); Serial.print(ens160.getMinorRev());
+    Serial.print("."); Serial.println(ens160.getBuild());
 
-    if (ArduinoIDE) Serial.print("setup: ENS160 custom mode ");
+    Serial.print("setup: ENS160 custom mode ");
     ens160.initCustomMode(3);                                     // Create custom sequence with three steps
     ens160.addCustomStep(50, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 50ms, no measurments, all hotplates at low temperatures 
     ens160.addCustomStep(350, 1, 1, 1, 1, 160, 215, 215, 200);    // Step 2: 350ms, no measurments, all hotplates at medium temperatures 
     ens160.addCustomStep(600, 1, 1, 1, 1, 250, 350, 350, 325);    // Step 3: 600ms, measurments done, all hotplates at high temperatures 
     if (!ens160.setMode(ENS160_OPMODE_CUSTOM) ) {
-      if (ArduinoIDE) Serial.println("FAILED");
+      Serial.println("FAILED");
     } else {
-      if (ArduinoIDE) Serial.println("successful");
+      Serial.println("successful");
     }
   }
 }
@@ -75,12 +71,10 @@ void loop() {
   if (ens160.available()) {
     ens160.measure();
   
-    if (ArduinoIDE) {
-      Serial.print("R HP0: ");Serial.print(ens160.getHP0());Serial.print("Ohm\t");
-      Serial.print("R HP1: ");Serial.print(ens160.getHP1());Serial.print("Ohm\t");
-      Serial.print("R HP2: ");Serial.print(ens160.getHP2());Serial.print("Ohm\t");
-      Serial.print("R HP3: ");Serial.print(ens160.getHP3());Serial.println("Ohm");
-    } 
+    Serial.print("R HP0: ");Serial.print(ens160.getHP0());Serial.print("Ohm\t");
+    Serial.print("R HP1: ");Serial.print(ens160.getHP1());Serial.print("Ohm\t");
+    Serial.print("R HP2: ");Serial.print(ens160.getHP2());Serial.print("Ohm\t");
+    Serial.print("R HP3: ");Serial.print(ens160.getHP3());Serial.println("Ohm");
   }
   delay(1000);
 }
