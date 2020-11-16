8ccc4d1f30ff41fc6aad857ab2ba0d358a44edd5
patryk02 <patryk.hallek@gmail.com>
2020-11-16 11:05:41 +0100

    Update ens160basic_normal.ino
    The OPMODE_NORMAL changed to OPMODE_STD.

--- examples/ens160basic_normal/ens160basic_normal.ino
+++ examples/ens160basic_normal/ens160basic_normal.ino
@@ -48,8 +48,8 @@ void setup() {
     Serial.print("."); Serial.print(ens160.getMinorRev());
     Serial.print("."); Serial.println(ens160.getBuild());
   
-    Serial.print("setup: ENS160 normal mode ");
-    if (!ens160.setMode(ENS160_OPMODE_NORMAL) ) {
+    Serial.print("setup: ENS160 standard mode ");
+    if (!ens160.setMode(ENS160_OPMODE_STD) ) {
       Serial.println("FAILED");
     } else {
       Serial.println("successful");
