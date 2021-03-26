a32401a5ef364a63cc62ef216c4a24e44bded552
nordicp <54993594+nordicp@users.noreply.github.com>
2021-03-26 15:24:24 +0100

    Update ens160basic_custom.ino

--- examples/ens160basic_custom/ens160basic_custom.ino
+++ examples/ens160basic_custom/ens160basic_custom.ino
@@ -50,8 +50,9 @@ void setup() {
 
     Serial.print("setup: ENS160 custom mode ");
     ens160.initCustomMode(3);                                     // Create custom sequence with three steps
-    ens160.addCustomStep(50, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 50ms, no measurments, all hotplates at low temperatures 
-    ens160.addCustomStep(350, 1, 1, 1, 1, 160, 215, 215, 200);    // Step 2: 350ms, measurments done, all hotplates at medium temperatures 
+                                                                  // time is a multiple of 24ms, 0 = no measurement, 1 = measurement, temperatures in Celsius
+    ens160.addCustomStep(48, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 48ms, no measurments, all hotplates at low temperatures 
+    ens160.addCustomStep(360, 1, 1, 1, 1, 160, 215, 215, 200);    // Step 2: 360ms, measurments done, all hotplates at medium temperatures 
     ens160.addCustomStep(600, 1, 1, 1, 1, 250, 350, 350, 325);    // Step 3: 600ms, measurments done, all hotplates at high temperatures 
     if (!ens160.setMode(ENS160_OPMODE_CUSTOM) ) {
       Serial.println("FAILED");
