d631b0e25846eb126cb852d3ee20d353a651da2f
nordicp <54993594+nordicp@users.noreply.github.com>
2021-11-25 12:19:32 +0100

    Update ens160basic_custom.ino

--- examples/ens160basic_custom/ens160basic_custom.ino
+++ examples/ens160basic_custom/ens160basic_custom.ino
@@ -3,7 +3,7 @@
   
   This is an example for ENS160 basic reading in custom mode
     
-  Updated by Sciosense / 29-July-2021
+  Updated by Sciosense / 25-Nov-2021
  ***************************************************************************/
 
 #include <Wire.h>
@@ -48,9 +48,9 @@ void setup() {
     Serial.print("."); Serial.println(ens160.getBuild());
 
     Serial.print("\tCustom mode ");
-    ens160.initCustomMode(3);                                     // Create custom sequence with three steps
-    ens160.addCustomStep(50, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 50ms, no measurments, all hotplates at low temperatures 
-    ens160.addCustomStep(350, 1, 1, 1, 1, 160, 215, 215, 200);    // Step 2: 350ms, no measurments, all hotplates at medium temperatures 
+    ens160.initCustomMode(3);                                     // Create custom sequence with three steps, step time is a multiple of 24ms
+    ens160.addCustomStep(48, 0, 0, 0, 0, 80, 80, 80, 80);         // Step 1: 48ms, no measurments, all hotplates at low temperatures 
+    ens160.addCustomStep(196, 0, 0, 0, 0, 160, 215, 215, 200);    // Step 2: 196ms, no measurments, all hotplates at medium temperatures 
     ens160.addCustomStep(600, 1, 1, 1, 1, 250, 350, 350, 325);    // Step 3: 600ms, measurments done, all hotplates at high temperatures 
     Serial.print(ens160.setMode(ENS160_OPMODE_CUSTOM) ? "done." : "failed!");
   }
